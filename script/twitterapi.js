b2107198f6b6d8a88dfebf22f8281cbad6ce0699
Renan Oliveira <renan@corp.globo.com>
2010-10-01 19:31:28 -0300

    geocode

--- script/twitterapi.js
+++ script/twitterapi.js
@@ -5,7 +5,8 @@ $(function(){
 				"q":"globo",
 				"rpp": "100",
 				"lang": "pt-BR",
-				"result_type" : "mixed"
+				"result_type" : "mixed",
+				"geocode" : "-23.559326,-46.658807,300km"
 			},
 			function(data, textStatus){
 				$.each(data.results, function(index, item){
